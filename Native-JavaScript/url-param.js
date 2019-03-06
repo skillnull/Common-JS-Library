472ce5792f8c681d16f8a1098dcfbeab7579436e
skillnull <skillisnull@gmail.com>
2019-03-06 14:09:09 +0800

    add url-param

---
+++ Native-JavaScript/url-param.js
@@ -0,0 +1,12 @@
+/**
+ * 获取hash或者search参数值
+ * @param paramName
+ * @returns {any}
+ */
+function getParam (paramName) {
+    var searchResult = window.location.search.substr(1)
+    var hashResult =  window.location.hash.substr(2)
+    var result = searchResult || hashResult
+    var reg = new RegExp("(^|&)" + paramName + "=([^&]*)(&|$)", "i")
+    return result.match(reg) !== null ? unescape(result.match(reg)[2]) : null
+}
