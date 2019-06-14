6f835ca635c71506eb987ad85cc4f6f85fb19d52
skillnull <skillisnull@gmail.com>
2019-06-14 21:13:53 +0800

    add trim all space function

--- Native-JavaScript/form-validator.js
+++ Native-JavaScript/form-validator.js
@@ -1,4 +1,5 @@
-export function checkEmail (val) { // 验证邮箱
+// 验证邮箱
+export function checkEmail (val) {
     val = trimSpace(val)
     let reg = new RegExp('^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$')
     if (reg.test(val)) {
@@ -8,7 +9,8 @@ export function checkEmail (val) { // 验证邮箱
     }
 }
 
-export function checkPhoneNumber (val) { // 验证手机号
+// 验证手机号
+export function checkPhoneNumber (val) {
     val = trimSpace(val)
     let reg = new RegExp('^(((13[0-9]{1})|(14[0-9]{1})|(17[0-9]{1})|(15[0-3]{1})|(15[4-9]{1})|(18[0-9]{1})|(199))+\\d{8})$')
     if (reg.test(val)) {
@@ -18,7 +20,8 @@ export function checkPhoneNumber (val) { // 验证手机号
     }
 }
 
-export function trimSpace (str) { // 去除字符串前后空格
+// 去除字符串前后空格
+export function trimSpace (str) {
     if (str) {
         return str.replace(/(^\s*)|(\s*$)/g, '')
     } else {
@@ -26,7 +29,17 @@ export function trimSpace (str) { // 去除字符串前后空格
     }
 }
 
-export function preloadImg (srcArr) { // 图片的预加载
+// 去除字符串所有空格
+export function trimAllSpace (str) {
+    if (str) {
+        return str.replace(/\s+/g, "")
+    } else {
+        return str
+    }
+}
+
+// 图片的预加载
+export function preloadImg (srcArr) {
     if (srcArr instanceof Array) {
         for (var i = 0; i < srcArr.length; i++) {
             var oImg = new Image()
@@ -35,7 +48,8 @@ export function preloadImg (srcArr) { // 图片的预加载
     }
 }
 
-export function verifyPassword (str) { // 验证密码，密码为6-12位字母数字或符号最少两种组合,特殊符号为 ~!@#$%^&*.,
+// 验证密码，密码为6-12位字母数字或符号最少两种组合,特殊符号为 ~!@#$%^&*.,
+export function verifyPassword (str) {
     str = trimSpace(str)
     let RegExp = /((?=.*[a-z])(?=.*\d)|(?=.*[a-z])(?=.*[~!@#$%^&*.,])|(?=.*\d)(?=.*[~!@#$%^&*.,]))[a-z\d~!@#$%^&*.,]{6,12}/i
     return RegExp.test(str)
